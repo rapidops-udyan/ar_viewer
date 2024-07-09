0f91f51f05d45ed513e8e499af98647780ecd336
udyan.singh <udyan.singh@rapidops.co>
2024-07-09 17:13:30 +0530

    Fixed plugin

--- android/src/main/kotlin/com/rapidops/ar_viewer/ArViewerPlugin.kt
+++ android/src/main/kotlin/com/rapidops/ar_viewer/ArViewerPlugin.kt
@@ -3,42 +3,60 @@ package com.rapidops.ar_viewer
 import android.content.Context
 import android.content.Intent
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 
 /** ArViewerPlugin */
-class ArViewerPlugin : FlutterPlugin, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
+class ArViewerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     private lateinit var channel: MethodChannel
     private var context: Context? = null
 
-    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        context = flutterPluginBinding.applicationContext
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ar_viewer")
         channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
     }
 
-    override fun onMethodCall(call: MethodCall, result: Result) {
-        if (call.method == "loadModel") {
-            context?.let {
-                val intent = Intent(it, ArViewerActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
-                it.startActivity(intent)
-                result.success(null)
-            } ?: result.error("NO_CONTEXT", "Unable to start ArViewerActivity", null)
-        } else {
-            result.notImplemented()
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "loadModel" -> {
+                context?.let {
+                    val intent = Intent(it, ArViewerActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
+                    it.startActivity(intent)
+                    result.success(null)
+                } ?: result.error("NO_CONTEXT", "Unable to start ArViewerActivity", null)
+            }
+
+            else -> {
+                result.notImplemented()
+            }
         }
     }
 
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+        context = null
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        context = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        context = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        context = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+        context = null
     }
 }
